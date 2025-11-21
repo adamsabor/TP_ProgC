7acb007ea5d4a617878c702adc27ddd4e03e46bc
adamsabor <156083054+adamsabor@users.noreply.github.com>
2025-11-21 11:41:53 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;
+    int num2 = 4;
+    char op = '+';
+
+    switch (op) {
+        case '+': printf("%d\n", num1 + num2); break;
+        case '-': printf("%d\n", num1 - num2); break;
+        case '*': printf("%d\n", num1 * num2); break;
+        case '/': 
+            if (num2 != 0) printf("%d\n", num1 / num2);
+            else printf("Erreur : division par zero\n");
+            break;
+        case '%': printf("%d\n", num1 % num2); break;
+        case '&': printf("%d\n", num1 & num2); break;
+        case '|': printf("%d\n", num1 | num2); break;
+        case '~': printf("%d\n", ~num1); break;
+        default: printf("Operateur inconnu\n");
+    }
+
+    return 0;
+}
+
 
